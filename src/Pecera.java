4696b227f525ea3faf3e6a8c5353ad1c00a46b90
Jon <Jon@192.168.1.151>
2026-04-18 11:49:15 +0200

    actualizacion ejercicio 1

--- src/Pecera.java
+++ src/Pecera.java
@@ -1,31 +1,7 @@
 
 public class Pecera {
 
-	/*
-	public class PeceraC{
-		
-		String nombre = "Acuario Nemo";
-		int capacidad = 200;  //litros
-		static double temperatura = 24.5;
-		boolean luzEncendida = true;
-		
-		double capacidad80 = capacidad/80*100;
 
-		PeceraC(String nombre, int capacidad,double temperatura,boolean luzEncendida) {
-			this.nombre = nombre;
-			this.capacidad = capacidad;
-			this.temperatura = temperatura;
-			this.luzEncendida = luzEncendida;
-		}
-		
-		public void info() {
-			System.out.println(nombre + );
-		}
-		
-		
-		
-	}
-	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
