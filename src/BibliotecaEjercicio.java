9fbcd3727b8ae213ea879a47a13192f37f4e9eb5
Jon <Jon@192.168.1.151>
2026-04-20 21:02:03 +0200

    EjercicioBiblioteca

---
+++ src/BibliotecaEjercicio.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class BibliotecaEjercicio {
+	static class Biblioteca{
+		
+		private String nombre;
+		private ArrayList<String> libros;
+		private ArrayList<String> autores;
+		
+		public String getNombre(String nombre) {
+			return nombre;
+		}
+		public void setNombre() {
+			this.nombre= nombre;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
