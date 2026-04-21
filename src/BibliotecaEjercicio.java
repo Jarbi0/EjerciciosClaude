f8e06d6fe3c30172c12cd74f712cedaf463abe55
Jon <Jon@192.168.1.151>
2026-04-21 20:23:15 +0200

    Ejercicio Biblioteca casi acabado

--- src/BibliotecaEjercicio.java
+++ src/BibliotecaEjercicio.java
@@ -15,10 +15,38 @@ public class BibliotecaEjercicio {
 		}
 		
 		
+		public void añadirLibro(String libro, String autor) {
+			if (libro!=null && !libro.isEmpty() && autor!=null && !autor.isEmpty()) {
+				this.libros.addLast(libro);
+				this.autores.addLast(autor);
+				System.out.println("Hemos añadido el libro y  su autor.");
+			} else System.out.println("Libro o autor incorrectos");
+		}
 		
 		
+		public void eliminarLibro(String libro) {
+			int indice = libros.indexOf(libro);
+			if (indice!=-1) {
+				libros.remove(indice);
+				autores.remove(indice);
+				System.out.println("Libro y autor eliminados.");
+			} else System.out.println("Ese libro no se encuentra en nuestros archivos");
+			}
 		
 		
+		public void buscarAutor(String libro) {
+			int indice = libros.indexOf(libro);
+			if (indice!=-1) {
+				System.out.println("El autor de este libro es: " + autores.get(indice));
+			} else System.out.println("Ese libro no se encuentra en nuestros archivos");
+			}
+		
+		
+		public void mostrarLibros() {
+			for (int i=0;i<libros.size();i++) {
+				System.out.println("Libro: " + libros.get(i) + " pertenece al autor: " + autores.get(i));
+				}
+			}
 		
 		
 	}
@@ -27,7 +55,7 @@ public class BibliotecaEjercicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
 		
 	}
 
