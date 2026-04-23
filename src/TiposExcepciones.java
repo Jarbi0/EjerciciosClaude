58d67b93570fe55a8439587919054dea8dd31120
Jon <Jon@192.168.1.151>
2026-04-23 20:22:06 +0200

    Excepciones:

---
+++ src/TiposExcepciones.java
@@ -0,0 +1,21 @@
+
+public class TiposExcepciones {
+/*
+	e.getMessage()
+	
+	
+	IllegalArgumentException		El argumento recibido no es válido
+	
+	NullPointerException			Intentas usar algo que es null
+	
+	ArrayIndexOutOfBoundsException  Accedes a un índice que no existe
+	
+	ArithmeticException				Error matemático, como dividir entre 0
+	
+	NumberFormatException			Intentas convertir "abc" a número
+	
+	
+	
+	
+*/	
+}
